55ba569da45faeb1dbf58e35aea9bf911252b9c5
rp4191 <rmrp50back@gmail.com>
2025-11-14 18:55:50 -0500

    Push1

--- calculator.py
+++ calculator.py
@@ -5,7 +5,31 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+import math
+
 def add(a, b): 
-    pass
+    return a+ b
+
+def sub(a, b):
+    return a - b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    if a == 0:
+        raise ZeroDivisionError
+    else:
+        return b / a
+    
+def log(a,b):
+    if a == 0:
+        raise ValueError
+    else:
+        return math.log(a,b)
+
+def exp(a,b):
+    return a**b
+    return a+ b
 
 
