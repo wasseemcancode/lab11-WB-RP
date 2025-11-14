38707ffcc73b4d3e9202a90b11c8bb9431870bfa
wasseemcancode <wasseemb2007@gmail.com>
2025-11-14 18:56:57 -0500

    wasem

--- calculator.py
+++ calculator.py
@@ -1,11 +1,57 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
 
-One function per operation, in order.
-"""
-# First example
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    else:
+        return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+
 def add(a, b): 
-    pass
+    return a+ b
 
+def sub(a, b):
+    return a - b
 
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    if a == 0:
+        raise ZeroDivisionError
+    else:
+        return b / a
+    
+def log(a,b):
+    if a == 0:
+        raise ValueError
+    else:
+        return math.log(a,b)
+
+def exp(a,b):
+    return a**b
+    return a+ b
+
+def sub(a, b):
+    return a - b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    if a == 0:
+        raise ZeroDivisionError
+    else:
+        return b / a
+    
+def log(a,b):
+    if a == 0:
+        raise ValueError
+    else:
+        return math.log(a,b)
+
+def exp(a,b):
+    return a**b
