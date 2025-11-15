8fb7403c35f5f91d43da818c952db6b6d1969c44
rp4191 <rmrp50back@gmail.com>
2025-11-14 19:40:23 -0500

    PushF

--- test_calculator.py
+++ test_calculator.py
@@ -2,54 +2,25 @@ import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self):
+        self.assertEqual(add(4, 5), 9)
+        self.assertEqual(add(-2, 4), 2)
+        self.assertEqual(add(-2, -4), -6)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide_by_zero(self): 
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 3)
+        
+    def test_logarithm(self): 
+        self.assertAlmostEqual(log(100, 10), 2)
+        self.assertAlmostEqual(log(27, 3), 3)
+        self.assertAlmostEqual(log(49, 7), 2)
+        
+    def test_log_invalid_base(self): 
+        with self.assertRaises(ValueError):
+            log(5, 0)
 
-    ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
-
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
-
-    ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
-
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
-
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
-    
-    ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
-
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
-
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+  
 
 # Do not touch this
 if __name__ == "__main__":
